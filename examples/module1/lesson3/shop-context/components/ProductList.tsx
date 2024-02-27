426da5f2e656c65e3f2eab9f83cd49c321b3199c
jakub <j.pawelski.it@gmail.com>
2024-02-27 11:55:50 +0100

    Improved performance by eliminating excessive overfitting

---
+++ examples/module1/lesson3/shop-context/components/ProductList.tsx
@@ -0,0 +1,25 @@
+import { memo } from 'react';
+import { Product as ProductType } from '../types/Product';
+import Product from '../components/Product';
+type ProductList = {
+  products: ProductType[];
+};
+
+export const ProductList = memo<ProductList>(({ products }) => {
+  return (
+    <div>
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h1 className="text-3xl font-semibold mb-10 text-center">
+            Explore Our Products
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 lg:mx-8 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
+            {products.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+});
