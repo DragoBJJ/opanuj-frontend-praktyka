28ec880eb3762b3168ad933986f5f37668300fe6
jakub <j.pawelski.it@gmail.com>
2024-02-27 15:45:47 +0100

    Support for downloading user data

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -1,45 +1,36 @@
-import React, { useEffect, useState } from 'react';
-
-interface User {
-  id: number;
-  name: string;
-}
-
-const API_URL = '/api/data/users?timeout=10000';
+import { useUserClient } from './hooks/useUserClient';
 
 const App = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const { users, getUsers, isLoading, errorMessage } = useUserClient();
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(({ users }) => {
-        setUsers(users);
-      });
-  }, []);
+  if (isLoading) return <p>is Loading.......</p>;
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between py-4">
         <h1 className="text-2xl font-bold">Users</h1>
         <div className="flex flex-row items-center">
-          <p className="mr-2">
-            Sorry, there seems to be connectivity issues...
-          </p>
-          <button className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
-            Try again
-          </button>
+          <p className="mr-2">{errorMessage && errorMessage}</p>
+          {errorMessage && (
+            <button
+              onClick={getUsers}
+              className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4"
+            >
+              Try again
+            </button>
+          )}
         </div>
       </div>
       <ul className="space-y-2">
-        {users.map((user, index) => (
-          <li
-            className="bg-white p-4 rounded-md border border-gray-100"
-            key={index}
-          >
-            {user.name}
-          </li>
-        ))}
+        {users.length > 0 &&
+          users.map((user, index) => (
+            <li
+              className="bg-white p-4 rounded-md border border-gray-100"
+              key={index}
+            >
+              {user.name}
+            </li>
+          ))}
       </ul>
     </div>
   );
