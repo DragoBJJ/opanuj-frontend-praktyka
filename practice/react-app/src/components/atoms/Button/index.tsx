af1cee5bab3b8ece10786076e58e8224972e4fd7
jakub <j.pawelski.it@gmail.com>
2024-02-29 12:01:48 +0100

    Added with and height to elements

--- practice/react-app/src/components/atoms/Button/index.tsx
+++ practice/react-app/src/components/atoms/Button/index.tsx
@@ -8,7 +8,7 @@ type InputType = {
 export const Button = memo<InputType>(({ name, onClick }) => {
   return (
     <button
-      className="flex mt-4 border-[1px] ease-in-out duration-300 hover:bg-black hover:text-white hover:border-black bg-white !outline-none border-black"
+      className="flex mt-4 w-[160px] h-[48px] mx-auto  border-[1px] justify-center items-center ease-in-out duration-300 hover:bg-black hover:text-white hover:border-black bg-white !outline-none border-black"
       onClick={onClick as unknown as MouseEventHandler<HTMLButtonElement>}
     >
       {name}
